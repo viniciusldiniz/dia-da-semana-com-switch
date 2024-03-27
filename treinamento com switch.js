bd3c348b89b23c4bb856d41fd8e0e4f5b79fd94b
Vinicius L. Diniz <42078940+viniciusldiniz@users.noreply.github.com>
2024-03-27 01:30:09 -0300

    treinamento com switch.js

--- treinamento com switch.js
+++ treinamento com switch.js
@@ -9,29 +9,31 @@ var agora = new Date()
  5 = Sexta
  6 = Sábado
  */
- console.log(diaSem) // 0 - Domingo a 6 - Sábado  
-switch(diaSem){// switch e case sao usados para fazer uma escolha de opções, se o valor da variável for igual a 0 ele vai para o case 0
-    case 0:// se o valor da variável for igual a 0, imprime "Domingo"
-        console.log('Domingo')// e para o switch de dia da semana
-        break// quebra o switch caso o valor seja igual a 0
-    case 1:// se o valor da variável for igual a 1, imprime "Segunda"
-        console.log('Segunda')// e para o switch de dia da semana
-        break// quebra o switch caso o valor seja igual a 1 
-    case 2:// se o valor da variável for igual a 2, imprime "Terça"   
-        console.log('Terça')// e para o switch de dia da semana      
-        break
-    case 3:
-        console.log('Quarta')
-        break
-    case 4:
-        console.log('Quinta')
-        break
-    case 5:
-        console.log('Sexta')
-        break
-    case 6:
-        console.log('Sabado')
-    default:// se o valor da variável for diferente de 0, 1, 2, 3, 4, 5 e 6, imprime "[ERRO] Dia inválido!"
-        console.log('[ERRO] Dia inválido!')
-        break
-    }
+console.log(diaSem); // Imprime o valor de diaSem, que deve ser um número de 0 a 6 representando os dias da semana (0 - Domingo a 6 - Sábado)
+
+switch(diaSem) { // Inicia a estrutura de controle switch, onde diaSem será avaliado para diferentes casos
+    case 0: // Se o valor de diaSem for igual a 0, imprime "Domingo"
+        console.log('Domingo');
+        break; // Encerra o switch caso este caso seja correspondido
+    case 1: // Se o valor de diaSem for igual a 1, imprime "Segunda"
+        console.log('Segunda');
+        break; // Encerra o switch caso este caso seja correspondido
+    case 2: // Se o valor de diaSem for igual a 2, imprime "Terça"
+        console.log('Terça');
+        break; // Encerra o switch caso este caso seja correspondido
+    case 3: // Se o valor de diaSem for igual a 3, imprime "Quarta"
+        console.log('Quarta');
+        break; // Encerra o switch caso este caso seja correspondido
+    case 4: // Se o valor de diaSem for igual a 4, imprime "Quinta"
+        console.log('Quinta');
+        break; // Encerra o switch caso este caso seja correspondido
+    case 5: // Se o valor de diaSem for igual a 5, imprime "Sexta"
+        console.log('Sexta');
+        break; // Encerra o switch caso este caso seja correspondido
+    case 6: // Se o valor de diaSem for igual a 6, imprime "Sábado"
+        console.log('Sábado');
+        break; // Encerra o switch caso este caso seja correspondido
+    default: // Caso nenhum dos casos acima seja correspondido (caso o valor de diaSem não seja 0 a 6)
+        console.log('[ERRO] Dia inválido!'); // Imprime "[ERRO] Dia inválido!"
+        break; // Encerra o switch
+}
